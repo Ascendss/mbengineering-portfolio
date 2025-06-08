0344f4a9630e7d9b56f32fa666775ac70647172a
Ascendss <redskinsfan675@gmail.com>
2025-06-07 21:37:26 -0400

    Update getprojects.js

--- netlify/functions/getprojects.js
+++ netlify/functions/getprojects.js
@@ -4,22 +4,14 @@ const matter = require('gray-matter');
 
 exports.handler = async function () {
   try {
-    // Corrected relative path for Netlify deployment
     const projectsDirectory = path.resolve(__dirname, 'projects');
-    
-    if (!fs.existsSync(projectsDirectory)) {
-      throw new Error(`Directory not found: ${projectsDirectory}`);
-    }
-
     const filenames = fs.readdirSync(projectsDirectory);
-    const projects = filenames
-      .filter((filename) => filename.endsWith('.md'))
-      .map((filename) => {
-        const filePath = path.join(projectsDirectory, filename);
-        const fileContent = fs.readFileSync(filePath, 'utf8');
-        const { data } = matter(fileContent);
-        return data;
-      });
+    const projects = filenames.map((filename) => {
+      const filePath = path.join(projectsDirectory, filename);
+      const fileContent = fs.readFileSync(filePath, 'utf8');
+      const { data } = matter(fileContent);
+      return data;
+    });
 
     return {
       statusCode: 200,

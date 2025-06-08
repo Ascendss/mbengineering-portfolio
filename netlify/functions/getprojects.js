5ad97b8b6aeb59f4cde892e7fff7276ea93408f6
Ascendss <redskinsfan675@gmail.com>
2025-06-07 21:21:10 -0400

    Update getprojects.js

--- netlify/functions/getprojects.js
+++ netlify/functions/getprojects.js
@@ -1,26 +1,17 @@
-const fs = require("fs");
-const path = require("path");
-const matter = require("gray-matter");
+const fs = require('fs');
+const path = require('path');
+const matter = require('gray-matter');
 
-exports.handler = async function () {
+exports.handler = async function() {
   try {
-    const projectsDir = path.resolve(__dirname, "projects");
-    if (!fs.existsSync(projectsDir)) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: `Directory not found: ${projectsDir}` }),
-      };
-    }
-
-    const files = fs.readdirSync(projectsDir);
-    const projects = files
-      .filter(file => file.endsWith(".md"))
-      .map(file => {
-        const filePath = path.join(projectsDir, file);
-        const fileContents = fs.readFileSync(filePath, "utf8");
-        const { data } = matter(fileContents);
-        return data;
-      });
+    const projectsDirectory = path.join(__dirname, 'projects');
+    const filenames = fs.readdirSync(projectsDirectory);
+    const projects = filenames.map((filename) => {
+      const filePath = path.join(projectsDirectory, filename);
+      const fileContent = fs.readFileSync(filePath, 'utf8');
+      const { data } = matter(fileContent);
+      return data;
+    });
 
     return {
       statusCode: 200,

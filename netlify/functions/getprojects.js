552acb35b5356e8223d79876198dac4e9f656ab7
Ascendss <redskinsfan675@gmail.com>
2025-06-07 21:44:38 -0400

    Update getprojects.js

--- netlify/functions/getprojects.js
+++ netlify/functions/getprojects.js
@@ -4,9 +4,11 @@ const matter = require('gray-matter');
 
 exports.handler = async function () {
   try {
-const projectsDirectory = path.resolve(__dirname, '../../functions/projects');
+    const projectsDirectory = path.resolve(__dirname, '../../functions/projects');
     const filenames = fs.readdirSync(projectsDirectory);
-    const projects = filenames.map((filename) => {
+    const mdFiles = filenames.filter(file => file.endsWith('.md'));
+
+    const projects = mdFiles.map((filename) => {
       const filePath = path.join(projectsDirectory, filename);
       const fileContent = fs.readFileSync(filePath, 'utf8');
       const { data } = matter(fileContent);
@@ -15,6 +17,7 @@ const projectsDirectory = path.resolve(__dirname, '../../functions/projects');
 
     return {
       statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
       body: JSON.stringify(projects),
     };
   } catch (error) {
